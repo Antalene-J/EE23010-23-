c00c6abbf68bbbc0c2fe78efb4ca414c2d0118ed
Antalene_J <119392158+Antalene-J@users.noreply.github.com>
2023-08-25 21:50:56 +0530

    Update main.py

--- 2/codes/main.py
+++ 2/codes/main.py
@@ -1,10 +1,11 @@
 import random
 
+#efficient func
 def not_adj(desk):
-    return not any(desk[i] == "M" and desk[i + 1] == "M" for i in range(len(desk) - 1))
+    return "M M" not in " ".join(desk)
 
 def sim(n):
-    desks = ["M", "M", "E", "E", "E", "E"]
+    desks = ["M", "E", "E","M", "E", "E"]
     nonadj_count = sum(not_adj(random.sample(desks, len(desks))) for j in range(n))
     prob = nonadj_count/n
     return prob
@@ -12,4 +13,3 @@ def sim(n):
 n = 100000
 result = sim(n)
 print("The probability that the married couple will have nonadjacent desks is:",result)
-
